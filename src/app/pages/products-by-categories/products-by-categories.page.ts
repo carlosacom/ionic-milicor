362ba4c4947a154fde37fd206074686d2ff7aa85
Carlos Andres Perez <carlosaperez1997@gmail.com>
2022-03-28 16:04:33 -0500

    marca de productos

--- src/app/pages/products-by-categories/products-by-categories.page.ts
+++ src/app/pages/products-by-categories/products-by-categories.page.ts
@@ -9,6 +9,9 @@ import { BackendService } from 'src/app/services/backend.service';
 })
 export class ProductsByCategoriesPage {
   products = [];
+  marcas = [];
+  allProducts = [];
+  marcaForm = null;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -16,14 +19,28 @@ export class ProductsByCategoriesPage {
   ) {
     this.activatedRoute.params.subscribe(params => {
       this.getProducts(params.category);
+      this.marcas = [];
+      this.marcaForm = '';
     });
   }
 
   getProducts(category: string) {
     this.backend.getProducts(category).then(response => {
+      this.allProducts = response;
       this.products = response;
+      this.getMarcas();
     }).catch(error => {
       console.error(error);
     });
+  }
+  getMarcas() {
+    this.marcas = [...new Set(this.products.map(p => p.marca))];
+  }
+  changeMarca(event) {
+    if (event.detail.value) {
+      this.products = this.allProducts.filter(p => p.marca === event.detail.value);
+    } else {
+      this.products = this.allProducts;
+    }
   }
 }
